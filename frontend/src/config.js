2c1751d736c77c5d588958ab06c3e37729bd0d52
Nawaf <smn11789@gmail.com>
2025-03-28 21:36:57 +0300

    3

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-const API_URL = process.env.REACT_APP_API_URL || 'https://predictbattle-backend-production.up.railway.app/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://predictbattlev3-production.up.railway.app/api';
 
 export default {
   API_URL
